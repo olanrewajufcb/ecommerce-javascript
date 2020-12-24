45e50ebfca184f5665264de5d73a718d19391900
mac <mac@macs-MacBook-Air.local>
2020-12-24 09:36:19 +0100

    second frontend commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 const App = () => {
   return (
-   <>
-   <h1>Welcome to Samiwinto</h1>
-   </>
+   <Router>
+  <Header/>
+   <main className="py-3">
+     <Container>
+       <Route path='/' component={HomeScreen} exact/>
+       <Route path='/Product/:id' component={ProductScreen} />
+
+       </Container>
+     
+  </main>
+   <Footer/>
+   </Router>
   );
 }
 
